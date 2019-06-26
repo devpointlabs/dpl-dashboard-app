cbfaca55de857e991ecc69b6377a3a70f2f9a831
Chris McGivney <cmmcgivney84@gmail.com>
2019-06-26 16:17:39 -0600

    can't figure out how to store the key in the git ignore

--- client/src/components/youtube/YTKey.js
+++ client/src/components/youtube/YTKey.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY = 'AIzaSyC2gFpH8Bn5psllvUrEtVSd7mtcBO7Fd74'
+const KEY = ''
 
 export default axios.create({
   baseURL: 'http://www.googleapis.com/youtube/v3/',
