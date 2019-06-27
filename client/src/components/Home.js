133b9b52f20d32096162fbf38058af1135aaf821
Michael Brown <Vurclash@gmail.com>
2019-06-27 17:19:00 -0600

    added simple stylization, doing research into more complex css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -12,30 +12,30 @@ const Home = () => (
  
 
   <> 
-    <Grid celled >
-        <Grid.Row >
-          <Grid.Column width={4} >
-          <Header as="styledHeader">Quote of Day:</Header>
+    <Grid celled only="large screen" >
+        <Grid.Row display="flex">
+          <Grid.Column width={4} color="grey" >
+          <Header color="purple" inverted>Quote of Day:</Header>
           <Quote />
           </Grid.Column>
-          <Grid.Column width={8} >
-             <Header>Todays Topic:</Header>
+          <Grid.Column width={8} color="grey">
+             <Header color="purple" inverted>Todays Topic:</Header>
              add your component here
           </Grid.Column>
-          <Grid.Column width={4} >
-            <Header>Weather & Time</Header>
+          <Grid.Column width={4} color="grey">
+            <Header color="purple" inverted>Weather & Time</Header>
            <Weather/>
           </Grid.Column>
         </Grid.Row>
 
        <Grid.Row >
-          <Grid.Column width={5} >
-          <Header>Upcoming Events:</Header>
+          <Grid.Column width={5} color="purple">
+          <Header color="grey" inverted>Upcoming Events:</Header>
           <p>Events</p>
           <p>Events</p>
           <p>Events</p>
           </Grid.Column>
-         <Grid.Column width={11}>
+         <Grid.Column width={11} color="purple">
          <YouTube />
          </Grid.Column>
        </Grid.Row>
