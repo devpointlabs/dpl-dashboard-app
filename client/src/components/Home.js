7cb6569f89fbb04f014122bf1c43682b3fae8e01
Michael Brown <Vurclash@gmail.com>
2019-06-27 16:22:25 -0600

    attempted/failed stylization

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react"
 import Quote from './Quote'
 import Weather from './Weather'
 import YouTube from './YouTube'
+import styled from 'styled-components'
 import { Header, Grid,  } from "semantic-ui-react";
 
 
@@ -11,24 +12,24 @@ const Home = () => (
  
 
   <> 
-    <Grid celled>
-        <Grid.Row>
-          <Grid.Column width={4}>
-          <Header>Quote of Day:</Header>
+    <Grid celled >
+        <Grid.Row >
+          <Grid.Column width={4} >
+          <Header as="styledHeader">Quote of Day:</Header>
           <Quote />
           </Grid.Column>
-          <Grid.Column width={8}>
+          <Grid.Column width={8} >
              <Header>Todays Topic:</Header>
              add your component here
           </Grid.Column>
-          <Grid.Column width={4}>
+          <Grid.Column width={4} >
             <Header>Weather & Time</Header>
            <Weather/>
           </Grid.Column>
         </Grid.Row>
 
-       <Grid.Row>
-          <Grid.Column width={5}>
+       <Grid.Row >
+          <Grid.Column width={5} >
           <Header>Upcoming Events:</Header>
           <p>Events</p>
           <p>Events</p>
@@ -45,11 +46,11 @@ const Home = () => (
          
  </>
 
+ 
+
 
 );
 
 
-const style = () => {
-  
-}
+
 export default Home;
