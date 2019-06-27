0ae30bff251057d80f04dc15b3edd471067b614a
Chris McGivney <cmmcgivney84@gmail.com>
2019-06-27 10:38:54 -0600

    merge commit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,20 +9,25 @@ const Home = () => (
         <Grid.Row>
           <Grid.Column width={4}>
           <Header>Quote of Day:</Header>
+          you can add your stuff here
           </Grid.Column>
           <Grid.Column width={8}>
              <Header>Todays Topic:</Header>
-             
+             add your component here
           </Grid.Column>
           <Grid.Column width={4}>
             <Header>Weather & Time</Header>
             <Image src='https://picsum.photos/200' />
+            take out the silly image and add your stuff here
           </Grid.Column>
         </Grid.Row>
 
        <Grid.Row>
           <Grid.Column width={5}>
           <Header>Upcoming Events:</Header>
+          <p>Events</p>
+          <p>Events</p>
+          <p>Events</p>
           </Grid.Column>
          <Grid.Column width={11}>
          <YouTube />
