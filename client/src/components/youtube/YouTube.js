4e8378c53d044bd3879dd5dab50d937a01eaaa0f
Chris McGivney <cmmcgivney84@gmail.com>
2019-07-16 16:46:50 -0600

    added date loging to display topic

--- client/src/components/youtube/YouTube.js
+++ client/src/components/youtube/YouTube.js
@@ -15,7 +15,7 @@ class YouTube extends React.Component {
            type="text/html" 
            width="1920" 
            height="1080"
-           src="https://www.youtube.com/embed/n5uz7egB9tA?autoplay=1&loop=1&modestbranding=1"
+           src="https://www.youtube.com/embed?listType=playlist&list=PL50JhZpTWVPIbp_XsZ5mZKSbS19wYvOoD&autoplay=1&loop=1&modestbranding=1"
            frameBorder="0"
            allowFullScreen
            />
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_162150) do
+ActiveRecord::Schema.define(version: 2019_07_16_184627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2019_07_16_162150) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "date"
+    t.integer "week"
+    t.integer "day"
   end
 
   create_table "users", force: :cascade do |t|
