b76c8935ea4f8d24e27161ea154777409f1b0e01
Michael Brown <Vurclash@gmail.com>
2019-07-18 12:38:04 -0600

    toggled youtube

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -8,8 +8,6 @@ import { Header, Grid, Responsive, } from "semantic-ui-react"
 import Clock from './Clock'
 import DisplayTopic from './Topics/DisplayTopic'
 
-
-
 const CurrentDate = () => {
   var tempDate = new Date();
   var date = (tempDate.getMonth() + 1) + '/' + tempDate.getDate() + '/' + tempDate.getFullYear()
@@ -19,73 +17,107 @@ const CurrentDate = () => {
   );
 }
 
-const Home = () => (
+
+class Home extends React.Component {
+
+  state = {
+    showInsta: false
+  }
+
+  handleInsta = () => {
+    this.setState(state => ({
+      showInsta: !state.showInsta
+    }))
+  }
+
+  render() {
+    if (this.state.showInsta) {
+      return (
+
+        <div style={{ overflowX: "hidden" }}>
+        <Grid style={{ height: '100vh', width: '100vw', position: "absolute" }} margin="0px">
 
 
+          <Grid.Row textAlign={'center'} style={{ height: '30%' }} display="flex">
+            <Grid.Column width={5} color="black" textAlign={"left"}>
+              <CurrentDate />
+              <hr />
+              <DisplayTweet />
+            </Grid.Column>
 
-  <div style={{ overflowX: "hidden" }}>
-    <Grid style={{ height: '100vh', width: '100vw', position: "absolute" }} margin="0px">
+            <Grid.Column width={8} color="grey" >
+              <Header style={{ fontSize: "3em", }} color="black" inverted >Todays Topic:</Header>
+              <DisplayTopic />
+              <hr />
+              <Quote />
+
+            </Grid.Column>
+
+            <Grid.Column textAlign={'center'} width={3} color="black">
+              <Clock />
+              <hr />
+              <Weather />
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row style={{ height: '70%' }}>
+            <Grid.Column width={5} color="black">
+              <CalApp />
+              <button style={{ position: "absolute", zIndex: "1000", left: "150px", opacity: "0.0", cursor: "pointer", padding: "40px 40px" }}
+                onClick={this.handleInsta} ></button>
+            </Grid.Column>
+            <Grid.Column width={11} color="black">
+              <h1>Instagram API will go here</h1>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div >
+      )
+    } else {
+      return (
+        <div style={{ overflowX: "hidden" }}>
+          <Grid style={{ height: '100vh', width: '100vw', position: "absolute" }} margin="0px">
 
 
-      <Grid.Row textAlign={'center'} style={{ height: '30%' }} display="flex">
-        <Grid.Column width={5} color="black" textAlign={"left"}>
-          <CurrentDate />
-          <hr />
-          <DisplayTweet />
-        </Grid.Column>
+            <Grid.Row textAlign={'center'} style={{ height: '30%' }} display="flex">
+              <Grid.Column width={5} color="black" textAlign={"left"}>
+                <CurrentDate />
+                <hr />
+                <DisplayTweet />
+              </Grid.Column>
 
-        <Grid.Column width={8} color="grey" >
-          <Header style={{ fontSize: "3em", }} color="black" inverted >Todays Topic:</Header>
-          <DisplayTopic />
-          <hr />
-          <Quote />
+              <Grid.Column width={8} color="grey" >
+                <Header style={{ fontSize: "3em", }} color="black" inverted >Todays Topic:</Header>
+                <DisplayTopic />
+                <hr />
+                <Quote />
 
-        </Grid.Column>
+              </Grid.Column>
 
-        <Grid.Column textAlign={'center'} width={3} color="black">
-          <Clock />
-          <hr />
-          <Weather />
-        </Grid.Column>
-      </Grid.Row>
+              <Grid.Column textAlign={'center'} width={3} color="black">
+                <Clock />
+                <hr />
+                <Weather />
+              </Grid.Column>
+            </Grid.Row>
 
-      {/* <Grid.Row style={{ height: '70%' }} display="flex">
-        <Grid.Column width={5} color="black">
-          <CalApp />
-        </Grid.Column>
-        <Grid.Column width={11} color="black">
+            <Grid.Row style={{ height: '70%' }}>
+              <Grid.Column width={5} color="black">
+                <CalApp />
+                <button style={{ position: "absolute", zIndex: "1000", left: "150px", opacity: "0.0", cursor: "pointer", padding: '40px 40px' }}
+                  onClick={this.handleInsta} ></button>
+              </Grid.Column>
+              <Grid.Column width={11} color="black">
+                <YouTube />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </div >
+      )
+    }
+  }
+}
 
-          <YouTube />
-        </Grid.Column>
-      </Grid.Row> */}
-      {/* <Grid.Row>
-      <Grid.Column textAlign={'center'} width={3} color="black">
-        <Clock />
-        <Weather />
-      </Grid.Column>
-    </Grid.Row> */}
-
-    <Grid.Row style={{ height: '70%' }}>
-      <Grid.Column width={5} color="black">
-        <CalApp />
-      </Grid.Column>
-      <Grid.Column width={11} color="black">
-
-        <YouTube />
-      </Grid.Column>
-    </Grid.Row>
-
-    {/* <Grid.Row color="grey" style={{height: '11%'}}>
-         <Grid.Column textAlign={'center'} width={16}>
-          <Header color="black">
-          <DisplayTweet/>
-          </Header>
-          </Grid.Column>
-      </Grid.Row> */}
-     </Grid>
-     </div >
-
-)
 
 
 export default Home;
