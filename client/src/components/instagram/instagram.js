34b04f88895568316d243a7b03457516a9680236
Chris McGivney <cmmcgivney84@gmail.com>
2019-07-22 09:23:34 -0600

    youtube search is working now?

--- client/src/components/instagram/instagram.js
+++ client/src/components/instagram/instagram.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import axios from 'axios' 
-import { Grid, } from 'semantic-ui-react'
-import posed, {PoseGroup, } from 'react-pose'
+import { Grid, Image } from 'semantic-ui-react'
+import dplmtn from '../../images/DplMtn.jpg'
+import styled from 'styled-components'
 
-
-const Image = posed.img({
-  enter: { y: 0, opacity: 1 },
-  exit: { y: 50, opacity: 0 }
-})
+// { images.map(image => 
+//   <Image 
+//   className="image"
+//   data-key={image}
+//   src={image.images.thumbnail.url}
+//   key={image.id}
+//   width={image.width}
+//   height={image.height}
+//    />
+//   )}
+// <Image 
+// src={dplmtn} 
+// width= "150px"
+// height="150px"
+// />
 
 class Instagram extends React.Component {
 state = {
   images: [],
-  isVisible: false,
+  
 }
 
 componentDidMount() {
-  
   axios.get("https://api.instagram.com/v1/users/1341161557/media/recent/?access_token=1341161557.4dfa049.9df7c290ccaf45c9aacbb9ed4816d242")
   .then(res => {
     this.setState({images: res.data.data})
@@ -24,30 +34,67 @@ componentDidMount() {
   })
   .catch( err => {
     console.log(err.response)
-  })
-  
+  }) 
 }
 
 render() {
   const {images } = this.state
-  return (
-   
-      <Grid>
-              <PoseGroup>
-                { images.map(image => 
-                    <Image 
-                    className="image"
-                    data-key={image}
-                    src={image.images.thumbnail.url}
-                    key={image.id}
-                    width={image.width}
-                    height={image.height} />
-                    )}
-              </PoseGroup>
-            </Grid>
-  )
-}
+  // const pictures = images.forEach((image)=>console.log(image.thumbnail.url,image.id))
 
+  return (
+    <Grid>
+    <Grid.Row columns={6} style={{padding: "0px"}}>
+     
+      <Grid.Column style={{ padding: "0px 0px"}}>
+        <Image 
+        src={dplmtn}
+        width="150px"
+        height="150px"
+        padding="0px" />
+        />
+      </Grid.Column>
+  
+      <Grid.Column style={{ padding: "0px 0px"}}>
+        <Image 
+        src={dplmtn}
+        width="150px"
+        height="150px"
+        padding="0px" />
+      </Grid.Column>
+      <Grid.Column style={{ padding: "0px 0px"}}>
+        <Image 
+        src={dplmtn}
+        width="150px"
+        height="150px"
+        padding="0px" />
+      </Grid.Column>
+      <Grid.Column style={{ padding: "0px 0px"}}>
+        <Image 
+        src={dplmtn}
+        width="150px"
+        height="150px"
+        padding="0px" />
+      </Grid.Column>
+      <Grid.Column style={{ padding: "0px 0px"}}>
+        <Image 
+        src={dplmtn}
+        width="150px"
+        height="150px"
+        padding="0px" />
+      </Grid.Column>
+      <Grid.Column style={{ padding: "0px 0px"}}>
+        <Image 
+        src={dplmtn}
+        width="150px"
+        height="150px"
+        padding="0px" />
+      </Grid.Column>
+    </Grid.Row>
+   
+   
+  </Grid>  
+    )
+  }
 }
 
 export default Instagram
